75d966ae4e69851d19d05ef1a257d843b85e78a7
elazaro14 <44474164+elazaro14@users.noreply.github.com>
2025-11-16 21:34:42 +0300

    Update teacher.js

--- teacher.js
+++ teacher.js
@@ -5,3 +5,11 @@ function loadGradeForm() {
   // Render form for those subjects only
 }
 window.onload = loadGradeForm;
+function getGrade(score) {
+  if (score >= 75 && score <= 100) return "A";
+  else if (score >= 65 && score <= 74) return "B";
+  else if (score >= 45 && score <= 64) return "C";
+  else if (score >= 30 && score <= 44) return "D";
+  else if (score >= 0 && score <= 29) return "F";
+  else return "Invalid";
+}
